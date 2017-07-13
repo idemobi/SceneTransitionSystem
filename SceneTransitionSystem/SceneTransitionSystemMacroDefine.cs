804b2d6414a1c5aa2a1c6b96747e878956555fa4
kortex <jfcontart@idemobi.com>
2017-07-13 17:58:14 +0200

    [STEP] unity 2017 compatibility

--- SceneTransitionSystem/SceneTransitionSystemMacroDefine.cs
+++ SceneTransitionSystem/SceneTransitionSystemMacroDefine.cs
@@ -1,57 +1,98 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 #if UNITY_EDITOR
 using UnityEditor;
 using UnityEditor.Build;
 
-[InitializeOnLoad]
-public class SceneTransitionSystemMacroDefine :  IActiveBuildTargetChanged
+//=====================================================================================================================
+namespace BasicToolBox
 {
-	const string kMacro = "SCENE_TRANSITION_SYSTEM";
-	static SceneTransitionSystemMacroDefine kSharedInstance;
-
-	static SceneTransitionSystemMacroDefine ()
+	[InitializeOnLoad]
+	/// <summary>
+	/// Net worked data macro define.
+	/// </summary>
+	public class SceneTransitionSystemMacroDefine :  IActiveBuildTargetChanged
 	{
-		if (kSharedInstance == null) {
-			kSharedInstance = new SceneTransitionSystemMacroDefine ();
-			kSharedInstance.OnChangedPlatform ();
+		//-------------------------------------------------------------------------------------------------------------
+		/// <summary>
+		/// The macro to check this package.
+		/// </summary>
+		const string kMacro = "SCENE_TRANSITION_SYSTEM";
+		//-------------------------------------------------------------------------------------------------------------
+		/// <summary>
+		/// The shared instance used for this class.
+		/// </summary>
+		static SceneTransitionSystemMacroDefine kSharedInstance;
+		//-------------------------------------------------------------------------------------------------------------
+		/// <summary>
+		/// Initializes the <see cref="NetWorkedData.SceneTransitionSystemMacroDefine"/> class. 
+		/// Instance kSharedInstance to use it when method OnActiveBuildTargetChanged must be invoked
+		/// </summary>
+		static SceneTransitionSystemMacroDefine ()
+		{
+			if (kSharedInstance == null) {
+				kSharedInstance = new SceneTransitionSystemMacroDefine ();
+				kSharedInstance.OnChangedPlatform ();
+			}
 		}
-	}
-
-	public int callbackOrder { get { return 0; } }
-
-	public void OnActiveBuildTargetChanged (BuildTarget previousTarget, BuildTarget newTarget)
-	{
-		OnChangedPlatform ();
-	}
-
-	public  void OnChangedPlatform ()
-	{
-		InstallMacro (EditorUserBuildSettings.selectedBuildTargetGroup);
-	}
-
-	public void InstallMacro (BuildTargetGroup sBuildTarget)
-	{
-		if (PlayerSettings.GetScriptingDefineSymbolsForGroup (sBuildTarget).Contains (kMacro) == false) {
-			Debug.Log ("Install macro " + kMacro + " in " + sBuildTarget + " player settings");
-			PlayerSettings.SetScriptingDefineSymbolsForGroup (sBuildTarget, PlayerSettings.GetScriptingDefineSymbolsForGroup (sBuildTarget) + ";" + kMacro);
+		//-------------------------------------------------------------------------------------------------------------
+		/// <summary>
+		/// Gets the callback order for IActiveBuildTargetChanged
+		/// </summary>
+		/// <value>The callback order.</value>
+		public int callbackOrder { get { return 0; } }
+		//-------------------------------------------------------------------------------------------------------------
+		/// <summary>
+		/// Raises the active build target changed event.
+		/// </summary>
+		/// <param name="previousTarget">Previous target.</param>
+		/// <param name="newTarget">New target.</param>
+		public void OnActiveBuildTargetChanged (BuildTarget previousTarget, BuildTarget newTarget)
+		{
+			OnChangedPlatform ();
+		}
+		//-------------------------------------------------------------------------------------------------------------
+		/// <summary>
+		/// Raises the changed platform event.
+		/// </summary>
+		public void OnChangedPlatform ()
+		{
+			InstallMacro (EditorUserBuildSettings.selectedBuildTargetGroup);
+		}
+		//-------------------------------------------------------------------------------------------------------------
+		/// <summary>
+		/// Installs the macro.
+		/// </summary>
+		/// <param name="sBuildTarget">S build target.</param>
+		public void InstallMacro (BuildTargetGroup sBuildTarget)
+		{
 			if (PlayerSettings.GetScriptingDefineSymbolsForGroup (sBuildTarget).Contains (kMacro) == false) {
-				Debug.LogError ("Fail to install macro " + kMacro + " in " + sBuildTarget + " player settings!");
+				Debug.Log ("Install macro " + kMacro + " in " + sBuildTarget + " player settings");
+				PlayerSettings.SetScriptingDefineSymbolsForGroup (sBuildTarget, PlayerSettings.GetScriptingDefineSymbolsForGroup (sBuildTarget) + ";" + kMacro);
+				if (PlayerSettings.GetScriptingDefineSymbolsForGroup (sBuildTarget).Contains (kMacro) == false) {
+					Debug.LogError ("Fail to install macro " + kMacro + " in " + sBuildTarget + " player settings!");
+				}
 			}
 		}
-	}
-
-	public void InstallMacroAll ()
-	{
-		Array BuildTargetGroupsArray = Enum.GetValues (typeof(BuildTargetGroup));
-		foreach (BuildTargetGroup tBuildTarget in BuildTargetGroupsArray) {
-			if (tBuildTarget != BuildTargetGroup.Unknown) {
-				InstallMacro (tBuildTarget);
+		//-------------------------------------------------------------------------------------------------------------
+		/// <summary>
+		/// Installs the macro in all build target.
+		/// </summary>
+		public void InstallMacroAll ()
+		{
+			Array BuildTargetGroupsArray = Enum.GetValues (typeof(BuildTargetGroup));
+			foreach (BuildTargetGroup tBuildTarget in BuildTargetGroupsArray) {
+				if (tBuildTarget != BuildTargetGroup.Unknown) {
+					InstallMacro (tBuildTarget);
+				}
 			}
 		}
+		//-------------------------------------------------------------------------------------------------------------
 	}
 }
+//=====================================================================================================================
 #endif
