ce8243809a86cdc309a8c16bb9d067b577e18318
kortex <jfcontart@idemobi.com>
2017-07-23 17:49:24 +0200

    [ADD] Menu add Layout

---
+++ SceneTransitionSystem/STSConstants/STSConstants.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+using UnityEngine;
+
+//=====================================================================================================================
+namespace SceneTransitionSystem
+{
+	public partial class STSConstants
+	{
+		//-------------------------------------------------------------------------------------------------------------
+		// Idemobi alert Strings
+		public const string K_ALERT_IDEMOBI_TITLE = 				"SceneTransitionSystem";
+		public const string K_ALERT_IDEMOBI_MESSAGE = 				"SceneTransitionSystem is an idéMobi module to  Scene Transition System in your game.";
+		public const string K_ALERT_IDEMOBI_OK = 					"Thanks!";
+		public const string K_ALERT_IDEMOBI_SEE_DOC = 				"See online docs";
+		public const string K_ALERT_IDEMOBI_DOC_HTTP = 				"http://www.idemobi.com/scenetransitionsystem";
+		//		public const string K_ALERT_IDEMOBI_DOC_HTTP = 				"http://idemobi.com/scenetransitionsystem/";
+		//-------------------------------------------------------------------------------------------------------------
+		// Menu Strings
+		public const string K_MENU_BASE = 							"Window/";
+		public const string K_MENU_IDEMOBI = 						K_MENU_BASE+"Scene Transition System by ideMobi";
+
+		//-------------------------------------------------------------------------------------------------------------
+	}
+}
+//=====================================================================================================================
---
+++ SceneTransitionSystem/STSEditorMenu/STSEditorMenu.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+//=====================================================================================================================
+namespace SceneTransitionSystem
+{
+	/// <summary>
+	/// STS editor menu.
+	/// </summary>
+	public class STSEditorMenu 
+	{
+		//-------------------------------------------------------------------------------------------------------------
+		/// <summary>
+		/// Initializes a new instance of the <see cref="SceneTransitionSystem.STSEditorMenu"/> class.
+		/// </summary>
+		public STSEditorMenu ()
+		{
+		}
+		//-------------------------------------------------------------------------------------------------------------
+		/// <summary>
+		/// Idemobis the net worked data info show.
+		/// </summary>
+		[MenuItem (STSConstants.K_MENU_IDEMOBI)]
+		public static void IdemobiNetWorkedDataInfoShow()
+		{
+			if (EditorUtility.DisplayDialog (STSConstants.K_ALERT_IDEMOBI_TITLE,
+				STSConstants.K_ALERT_IDEMOBI_MESSAGE,
+				STSConstants.K_ALERT_IDEMOBI_OK,
+				STSConstants.K_ALERT_IDEMOBI_SEE_DOC)) {
+			} else {
+				Application.OpenURL (STSConstants.K_ALERT_IDEMOBI_DOC_HTTP);
+			}
+		}
+		//-------------------------------------------------------------------------------------------------------------
+	}
+}
+//=====================================================================================================================
+#endif
